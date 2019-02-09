2394d709190e3c0f9736d832b3dfb280dcab9f9e
Kevin Phillips <thefriendlycoder@gmail.com>
2019-02-09 16:49:56 -0400

    more build fixes

--- lock.sh
+++ lock.sh
@@ -2,13 +2,13 @@
 virtualenv -p python2 tmp
 source ./tmp/bin/activate
 pip install -e .[dev]
-pip freeze --exclude-editable > ./tests/python2_deps.txt
+pip freeze --exclude-editable > ./tests/python2.reqs
 deactivate
 rm -rf tmp
 
 virtualenv -p python3 tmp
 source ./tmp/bin/activate
 pip install -e .[dev]
-pip freeze --exclude-editable > ./tests/python3_deps.txt
+pip freeze --exclude-editable > ./tests/python3.reqs
 deactivate
 rm -rf tmp
